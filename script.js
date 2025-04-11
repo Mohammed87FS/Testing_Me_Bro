c77d471711606bdc3bba38ebb1d63d3674313cf4
Mohammed Ammar Al Hamadani <mohammedamaar165@gmail.com>
2025-04-11 20:29:03 +0200

    i made some progress.. first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const Zaid = "Zaid";
+const message = "Hello, " + Zaid + "! Welcome to the JavaScript world! I hate teamers";
+console.log(message);
+
+
+document.getElementById("welcome-message").textContent = message;
